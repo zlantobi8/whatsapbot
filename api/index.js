1086834d43556a8829196e06388f30d2926099f7
zlantobi8 <fagbuyirotobi4@gmail.com>
2025-08-15 09:35:19 -0700

    good

--- api/index.js
+++ api/index.js
@@ -7,11 +7,21 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    type: process.env.FIREBASE_TYPE,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    clientId: process.env.FIREBASE_CLIENT_ID,
+    authUri: process.env.FIREBASE_AUTH_URI,
+    tokenUri: process.env.FIREBASE_TOKEN_URI,
+    authProviderX509CertUrl: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+    clientC509CertUrl: process.env.FIREBASE_CLIENT_CERT_URL,
+    universeDomain: process.env.FIREBASE_UNIVERSE_DOMAIN
+  }),
 });
-
 
 const db = admin.firestore();
 db.settings({ ignoreUndefinedProperties: true });
