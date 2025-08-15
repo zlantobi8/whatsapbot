3214d9d0aeef85e22d1cc58321994772af2b8ad9
zlantobi8 <fagbuyirotobi4@gmail.com>
2025-08-15 04:19:19 -0700

    good

--- api/index.js
+++ api/index.js
@@ -5,14 +5,24 @@ import axios from 'axios';
 import serverless from 'serverless-http';
 
 // --- Firebase setup ---
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+dotenv.config();
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-  });
-}
+
+admin.initializeApp({
+  credential: admin.credential.cert({
+    type: process.env.FIREBASE_TYPE,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    clientId: process.env.FIREBASE_CLIENT_ID,
+    authUri: process.env.FIREBASE_AUTH_URI,
+    tokenUri: process.env.FIREBASE_TOKEN_URI,
+    authProviderX509CertUrl: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+    clientC509CertUrl: process.env.FIREBASE_CLIENT_CERT_URL,
+    universeDomain: process.env.FIREBASE_UNIVERSE_DOMAIN
+  }),
+});
 
 const db = admin.firestore();
 db.settings({ ignoreUndefinedProperties: true });
