6e16f56a24500eef18a6ee495767e7c58d1067fe
zlantobi8 <fagbuyirotobi4@gmail.com>
2025-08-18 08:18:09 -0700

    eer

--- api/dataplans.js
+++ api/dataplans.js
@@ -1,18 +1,15 @@
-// dataPlans.js
-const datas = require('./myjs.json'); // or use import datas from './myjs.json' if using ESM
+import datas from './myjs.json' assert { type: 'json' };
 
-// Export each network plan
-const MTN_PLAN = datas.MTN_PLAN;
-const GLO_PLAN = datas.GLO_PLAN;
-const AIRTEL_PLAN = datas.AIRTEL_PLAN;
-const MOBILE9_PLAN = datas["9MOBILE_PLAN"]; // 9MOBILE_PLAN key has number at start
+// Extract each network plan
+export const MTN_PLAN = datas.MTN_PLAN;
+export const GLO_PLAN = datas.GLO_PLAN;
+export const AIRTEL_PLAN = datas.AIRTEL_PLAN;
+export const MOBILE9_PLAN = datas['9MOBILE_PLAN'];
 
-// Map for easy lookup
-const DATA_PLANS = {
+// Map network IDs to their respective plans
+export const DATA_PLANS = {
   1: MTN_PLAN.ALL,
   2: AIRTEL_PLAN.ALL,
   3: GLO_PLAN.ALL,
   4: MOBILE9_PLAN.ALL
 };
-
-module.exports = { MTN_PLAN, GLO_PLAN, AIRTEL_PLAN, MOBILE9_PLAN, DATA_PLANS };
